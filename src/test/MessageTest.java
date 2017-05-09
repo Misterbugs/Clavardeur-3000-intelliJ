3438b90a90040c34b0f5ae588e5618f7e31b4f6b
Romain Riviere <rriviere@etud.insa-toulouse.fr>
2017-05-09 13:43:16 +0200

    Cleaning

--- src/test/MessageTest.java
+++ src/test/MessageTest.java
@@ -1,14 +1,20 @@
 package test;
 
+import com.sun.xml.internal.ws.developer.UsesJAXBContext;
 import controller.NetworkHandler;
 import message.Message;
 import message.MsgAskFile;
 import message.MsgFactory;
 import message.MsgText;
+import model.Address;
 import model.Model;
 import model.User;
+import model.UserList;
 import network.Network;
 import org.junit.Test;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 import static org.junit.Assert.assertNotNull;
 
@@ -48,13 +54,80 @@ public class MessageTest {
             e.printStackTrace();
         }
 
-        Message receivedMessage = Model.getInstance().getConversation(localUser.getFullUserName()).getMessageList().get(0);
+        Message receivedMessage = null;
+
+        for(Message m : Model.getInstance().getConversation(localUser.getFullUserName()).getMessageList()){
+            if(m.getNumMessage() == mesg.getNumMessage()){
+                receivedMessage = m;
+                break;
+            }
+        }
+
+        if(receivedMessage!=null){
+            assert (receivedMessage.getClass() == MsgText.class);
+            assert (((MsgText) receivedMessage).getTextMessage().equals(((MsgText) mesg).getTextMessage()));
+
+        }
+        else{
+            assert false;
+        }
+
+       // Message receivedMessage = Model.getInstance().getConversation(localUser.getFullUserName()).getMessageList().get(0);
 
 
-        System.out.println(receivedMessage.getClass());
-        assert (receivedMessage.getClass() == MsgText.class);
-        assert (((MsgText) receivedMessage).getTextMessage().equals(((MsgText) mesg).getTextMessage()));
 
     }
-    
+
+    @Test
+    public void messageNotDeliveredACK(){
+        User notPresentUser = null;
+        try {
+        notPresentUser = new User("NotPresent", new Address(InetAddress.getByName("10.0.0.0"), 2048), true);
+
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        UserList.getInstance().addUser(notPresentUser);
+        networkHandler.sendMessageACK(MsgFactory.createMessage(localUser, notPresentUser,"Test"),res ->{
+            if(res ==1){
+                assert false;
+            }
+            else{
+                assert true;
+            }
+        });
+
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void messageDeliveredACK(){
+
+        networkHandler.sendMessageACK(MsgFactory.createMessage(localUser,localUser,"Hello me!"), res ->{
+           if(res ==1){
+               assert true;
+           } else{
+               assert false;
+           }
+
+        });
+
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
 }
